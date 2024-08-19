f79f496de7952a260fa308ace86975ea59f4b263
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:55:24 +0300

    Update disassembly.py

--- jishaku/repl/disassembly.py
+++ jishaku/repl/disassembly.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.repl.disassembly
-~~~~~~~~~~~~~~~~~~~~~~~~
-
-Functions pertaining to the disassembly of Python code
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import ast
 import dis
