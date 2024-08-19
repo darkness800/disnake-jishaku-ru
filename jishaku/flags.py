334361c3376c096255d675a707be55767187eddb
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:49:43 +0300

    Update flags.py

--- jishaku/flags.py
+++ jishaku/flags.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.flags
-~~~~~~~~~~~~~~
-
-The Jishaku cog base, which contains most of the actual functionality of Jishaku.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import dataclasses
 import inspect
