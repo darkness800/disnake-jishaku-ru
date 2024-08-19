a69b553906c4cb2d86b00e5bb4e90d103b201b4a
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:57:22 +0300

    Update voice.py

--- jishaku/features/voice.py
+++ jishaku/features/voice.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.features.voice
-~~~~~~~~~~~~~~~~~~~~~~~
-
-The jishaku core voice-related commands.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import typing
 
