bde5f518a513002680dc24bf914a4ecc33cde40d
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 04:00:33 +0300

    Update invocation.py

--- jishaku/features/invocation.py
+++ jishaku/features/invocation.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.features.invocation
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The jishaku command invocation related commands.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import contextlib
 import inspect
