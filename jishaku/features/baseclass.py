0a6d81f81e3d6d6da9e559c29abd88df06c0f10f
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 04:01:50 +0300

    Update baseclass.py

--- jishaku/features/baseclass.py
+++ jishaku/features/baseclass.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.features.baseclass
-~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The base Feature class that serves as the superclass of all feature components.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import asyncio
 import collections
