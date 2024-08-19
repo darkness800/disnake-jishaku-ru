36039ba275362f8622f359e3da9e8bf0f53f3393
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:48:08 +0300

    Update hljs.py

--- jishaku/hljs.py
+++ jishaku/hljs.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.hljs
-~~~~~~~~~~~~
-
-Constants and functions related to syntax highlighting with highlight.js
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import re
 import typing
