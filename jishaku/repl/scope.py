998db2395f34429420bf2ae773f83f19577b6922
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:54:03 +0300

    Update scope.py

--- jishaku/repl/scope.py
+++ jishaku/repl/scope.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.repl.scope
-~~~~~~~~~~~~~~~~~~
-
-The Scope class and functions relating to it.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import inspect
 import typing
