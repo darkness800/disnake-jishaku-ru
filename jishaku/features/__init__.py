ba8737982f0afe1701fe896b6abc068147ac83d2
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 04:02:13 +0300

    Update __init__.py

--- jishaku/features/__init__.py
+++ jishaku/features/__init__.py
@@ -1,12 +1,2 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.features
-~~~~~~~~~~~~~~~~~
-
-The namespace containing the Feature components that go into the final Jishaku cog class.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
