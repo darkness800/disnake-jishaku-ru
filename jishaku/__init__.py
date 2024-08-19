df8ade98cafd131acdd6f32855b7732fc9f8ea65
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:51:47 +0300

    Update __init__.py

--- jishaku/__init__.py
+++ jishaku/__init__.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku
-~~~~~~~
-
-A disnake extension including useful tools for bot development and debugging.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 from jishaku.cog import *  # noqa: F401
 from jishaku.features.baseclass import Feature  # noqa: F401
