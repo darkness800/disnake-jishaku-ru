0607e9133eb4a5224f4e2d540a45d6bf1b1e8140
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:55:51 +0300

    Update compilation.py

--- jishaku/repl/compilation.py
+++ jishaku/repl/compilation.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.repl.compilation
-~~~~~~~~~~~~~~~~~~~~~~~~
-
-Constants, functions and classes related to classifying, compiling and executing Python code.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import ast
 import asyncio
