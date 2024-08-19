1ca72f12456cc6eb13faf06c6c9df65563d6ff35
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-19 03:56:43 +0300

    Update youtube.py

--- jishaku/features/youtube.py
+++ jishaku/features/youtube.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-"""
-jishaku.features.youtube
-~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The jishaku youtube-dl command.
-
-:copyright: (c) 2021 Devon (Gorialis) R
-:license: MIT, see LICENSE for more details.
-
-"""
+# SPDX-License-Identifier: MIT
 
 import disnake
 import youtube_dl
