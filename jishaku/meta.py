b5cf532ac3fe11f65457151d5ff369aaef04cdf2
darkness800 <161841069+darkness800@users.noreply.github.com>
2024-08-21 13:08:14 +0300

    Add files via upload

--- jishaku/meta.py
+++ jishaku/meta.py
@@ -17,7 +17,7 @@ __all__ = (
 
 # pylint: disable=invalid-name
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-version_info = VersionInfo(major=2, minor=6, micro=5, releaselevel='final', serial=0)
+version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel='final', serial=0)
 
 __author__ = 'Darkness800'
 __copyright__ = 'Copyright 2021 Devon (Gorialis) R, Copyright (c) 2024 MayaTeam'
